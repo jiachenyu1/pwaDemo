a616a5877a526730a09f1390b6655c2cc329cfd0
jiachenyu <jiachenyu@xin.com>
2018-12-12 21:42:18 +0800

    tijiao

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,6 +1,6 @@
 const offlineURL = '/offline';
 const staticFile = ["/", "/manifest.json", "/css/style.css", "/image/banner.png", "/image/logo/ic_launcher48.png", "/image/favicon.ico"].concat(offlineURL)
-const version = "v1.5";
+const version = "v2.0";
 const cache_host = [location.host, 'c.xinstatic.com'];
 
 self.addEventListener("install", function (e) {
@@ -28,13 +28,29 @@ self.addEventListener('activate', event => {
             return Promise.all(
                 keylist
                     .filter(key => key !== version)
-                    .map(key => caches.delete(key))
+                    .map(key => {
+                        caches.delete(key);
+                        self.clients.matchAll()
+                            .then(function (clients) {
+                                if (clients && clients.length) {
+                                    clients.forEach(function (client) {
+                                        // 发送字符串'sw.update'
+                                        client.postMessage('update');
+                                    })
+                                }
+                            })
+                    })
             );
 
         }).then(() => self.clients.claim())
     )
 
 });
+
+self.addEventListener("message", e => {
+    console.log(e);
+    e.source.postMessage('send')
+})
 
 self.addEventListener("fetch", e => {
     e.respondWith(
@@ -80,8 +96,15 @@ const handleFetchRequest = function (req) {
                         })
                         .catch(function (err) {
                             console.log('Update cache ' + request.url + ' fail: ' + err.message);
+                            return new Response(
+                                '<svg role="img" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title>offline</title><path d="M0 0h400v300H0z" fill="#eee" /><text x="200" y="150" text-anchor="middle" dominant-baseline="middle" font-family="sans-serif" font-size="50" fill="#ccc">offline</text></svg>',
+                                { headers: {
+                                        'Content-Type': 'image/svg+xml',
+                                        'Cache-Control': 'no-store'
+                                    }}
+                            );
                         });
-                    return response;
+                    // return response;
                 }
 
                 // Return fetch response
